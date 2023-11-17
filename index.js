0c793c7e840df4600ba3c9dcf4457435fdedd9c0
CodeForGhost <ilikeabdul7@gmail.com>
2023-11-17 14:58:45 +0530

    auth part added

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 dotenv.config();
 
-mongoose.connect(
-    process.env.MONGO_URL
-).then(() => {
-    console.log("db connected successfully")
-}).catch((e) => {
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("db connected successfully");
+  })
+  .catch((e) => {
     console.log(e);
-})
+  });
 
-
-app.use(express.json())
+app.use(express.json());
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log("Server is running");
-})
+  console.log("Server is running at port " + (process.env.PORT || 5000));
+});
